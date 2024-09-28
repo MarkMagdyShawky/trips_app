7c03219a90691a9521aaabd0e86338255309e57a
Mark <markmarkmagdy1010@gmail.com>
2024-09-28 14:04:45 +0300

    add new font

--- lib/core/resources/styles.dart
+++ lib/core/resources/styles.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
 
+abstract class Styles {
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle22 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyleLogo = TextStyle(
+    fontSize: 34,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Bangers',
+  );
+
+  static const textStyle42 = TextStyle(
+    fontSize: 42,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const textStyle48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w900,
+  );
+}
