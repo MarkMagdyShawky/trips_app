d0c24ab095c7e2c00767816aa28d2e11847a8ea7
Mark <markmarkmagdy1010@gmail.com>
2024-09-28 18:22:43 +0300

    add icons

--- lib/core/resources/image_manager.dart
+++ lib/core/resources/image_manager.dart
@@ -1,3 +1,6 @@
 class ImageManager {
   static String kAppLogo = 'assets/images/logo/Trips.svg';
+  static String kCalenderIcon = 'assets/images/icons/calendar.png';
+  static String kBeachIcon = 'assets/images/icons/beach.png';
+  static String kTravellingIcon = 'assets/images/icons/travelling.png';
 }
