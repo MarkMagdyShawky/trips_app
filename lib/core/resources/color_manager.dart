fbc80a452c664edeaacf507dae4b33e6285d0cde
Mark <markmarkmagdy1010@gmail.com>
2024-09-28 12:59:52 +0300

    add main colors

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kBlueColor = Color(0xff38B6FF);
+
+const Color kYellowColor = Color(0xffFFCC29);
+
+const Color kFontWhite = Colors.white;
+
+const Color kFontBlack = Colors.black;
