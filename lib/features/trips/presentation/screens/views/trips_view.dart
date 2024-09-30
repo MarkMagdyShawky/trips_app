7e78f187db66ce1e2f1d77a3c0348aafc91a2c29
Mark <markmarkmagdy1010@gmail.com>
2024-10-01 01:50:53 +0300

    add drawer and appBar

--- lib/features/trips/presentation/screens/views/trips_view.dart
+++ lib/features/trips/presentation/screens/views/trips_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:trips_app/core/widgets/custom_app_bar.dart';
+import 'package:trips_app/core/widgets/custom_drawer.dart';
+import 'package:trips_app/features/trips/presentation/screens/views/trips_view_body.dart';
 
 class TripsView extends StatelessWidget {
   const TripsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: CustomAppBar().customAppBar('Hurghada Trips'),
+      drawer: CustomDrawer(),
+      body: TripsViewBody(),
+    );
   }
 }
