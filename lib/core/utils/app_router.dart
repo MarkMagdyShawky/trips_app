f48cacc7b737596a2d6ef28ae077a983c7e7cb83
Mark <markmarkmagdy1010@gmail.com>
2024-09-30 04:30:32 +0300

    change path

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:trips_app/features/home/presentation/screen/view/home_view.dart';
 import 'package:trips_app/features/onboarding/presentation/screen/views/onboarding_view.dart';
 import 'package:trips_app/features/splash/presentation/screen/views/splash_view.dart';
 
@@ -12,10 +13,15 @@ class AppRouter {
       path: RoutesNames.kOnboardingView,
       builder: (context, state) => OnboardingView(),
     ),
+    GoRoute(
+      path: RoutesNames.kHomeView,
+      builder: (context, state) => HomeView(),
+    ),
   ]);
 }
 
 class RoutesNames {
   static const String kSplashView = "/Splash";
   static const String kOnboardingView = "/Onboarding";
+  static const String kHomeView = "/Home";
 }
