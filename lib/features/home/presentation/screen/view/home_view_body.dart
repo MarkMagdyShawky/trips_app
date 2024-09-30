8b4b4ac906b31681cc335d6c5dbe7757c084f3bf
Mark <markmarkmagdy1010@gmail.com>
2024-09-30 23:12:09 +0300

    add Custom Categories List View

--- lib/features/home/presentation/screen/view/home_view_body.dart
+++ lib/features/home/presentation/screen/view/home_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:trips_app/core/resources/image_manager.dart';
 import 'package:trips_app/core/resources/pageDimensions.dart';
-import 'package:trips_app/features/home/presentation/screen/widget/custom_category_card.dart';
+import 'package:trips_app/features/home/presentation/screen/widget/custom_categories_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,18 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return SizedBox(
       width: PageDimensions().pageWidth(context),
       height: PageDimensions().pageHeight(context),
-      child: ListView(
-        children: [
-          CustomCategoryCard(image: ImageManager.kTestImage, catigoryName: 'Hurghada'),
-          SizedBox(height: 35),
-          CustomCategoryCard(image: ImageManager.kTestImage, catigoryName: 'Hurghada'),
-          SizedBox(height: 35),
-          CustomCategoryCard(image: ImageManager.kTestImage, catigoryName: 'Hurghada'),
-          SizedBox(height: 35),
-          CustomCategoryCard(image: ImageManager.kTestImage, catigoryName: 'Hurghada'),
-          SizedBox(height: 35),
-        ],
-      ),
+      child: CustomCategoriesListView(),
     );
   }
 }
