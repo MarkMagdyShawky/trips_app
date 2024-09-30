0a638a2abffc4aeb9fe1da66f008bbbc5d6adec4
Mark <markmarkmagdy1010@gmail.com>
2024-09-30 04:27:57 +0300

    add new color

--- lib/core/resources/color_manager.dart
+++ lib/core/resources/color_manager.dart
@@ -4,6 +4,8 @@ const Color kBlueColor = Color(0xff38B6FF);
 
 const Color kYellowColor = Color(0xffFFCC29);
 
+const Color kYellowColor2 = Color.fromARGB(255, 255, 224, 123);
+
 const Color kWhiteColor = Colors.white;
 
 const Color kBlackColor = Colors.black;
