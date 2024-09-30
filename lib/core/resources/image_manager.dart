34cccebdb02408576031a1cbf51bf107836c7188
Mark <markmarkmagdy1010@gmail.com>
2024-09-30 04:27:21 +0300

    add new image

--- lib/core/resources/image_manager.dart
+++ lib/core/resources/image_manager.dart
@@ -3,4 +3,5 @@ class ImageManager {
   static String kCalenderIcon = 'assets/images/icons/calendar.png';
   static String kBeachIcon = 'assets/images/icons/beach.png';
   static String kTravellingIcon = 'assets/images/icons/travelling.png';
+  static String kTestImage = 'assets/images/test_image.jpg';
 }
