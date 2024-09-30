a5db967b1d0644b23cc4fad82e6e26fe14b7e604
Mark <markmarkmagdy1010@gmail.com>
2024-09-30 04:30:10 +0300

    Custom Category Card

---
+++ lib/features/home/presentation/screen/widget/custom_category_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:trips_app/core/functions/gradient_functions.dart';
+import 'package:trips_app/core/resources/color_manager.dart';
+import 'package:trips_app/core/resources/image_manager.dart';
+import 'package:trips_app/core/resources/pageDimensions.dart';
+import 'package:trips_app/core/resources/styles.dart';
+
+class CustomCategoryCard extends StatelessWidget {
+  const CustomCategoryCard({super.key, required this.image, required this.catigoryName});
+  final String image;
+  final String catigoryName;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        height: PageDimensions().pageHeight(context) / 4.2,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+        ),
+        child: Container(
+          height: PageDimensions().pageHeight(context) / 4.2,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            gradient: LinearGradientbackground([kBlueColor.withOpacity(0.7), kYellowColor2.withOpacity(0.7)]),
+          ),
+          child: Text(
+            catigoryName,
+            style: Styles.textStyle36,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
