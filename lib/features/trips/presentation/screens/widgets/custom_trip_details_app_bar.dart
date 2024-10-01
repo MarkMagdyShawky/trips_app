1d4d3a3877ae6270ae3d19101ffa1cdf1c3e5b5d
Mark <markmarkmagdy1010@gmail.com>
2024-10-01 18:32:05 +0300

    custom tio details app bar

---
+++ lib/features/trips/presentation/screens/widgets/custom_trip_details_app_bar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:trips_app/core/resources/color_manager.dart';
+import 'package:trips_app/core/resources/styles.dart';
+
+class CustomTripDetailsAppBar extends StatelessWidget {
+  const CustomTripDetailsAppBar({
+    super.key,
+    required this.title,
+  });
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 40),
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {
+              GoRouter.of(context).pop();
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              size: 30,
+              color: kBlackColor,
+            ),
+          ),
+          SizedBox(width: 10),
+          Text(
+            title,
+            style: Styles.textStyle22.copyWith(fontFamily: 'Bangers'),
+          ),
+        ],
+      ),
+    );
+  }
+}
