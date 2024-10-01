2d19fce09692d467a95f0738074463eabe792752
Mark <markmarkmagdy1010@gmail.com>
2024-10-02 00:24:46 +0300

    add new images

--- lib/core/resources/image_manager.dart
+++ lib/core/resources/image_manager.dart
@@ -4,4 +4,10 @@ class ImageManager {
   static String kBeachIcon = 'assets/images/icons/beach.png';
   static String kTravellingIcon = 'assets/images/icons/travelling.png';
   static String kTestImage = 'assets/images/test_image.jpg';
+  static String kTestImage2 = 'assets/images/test_image2.jpg';
+
+  static String kPersonIcon = 'assets/images/icons/person.gif';
+  static String kClockIcon = 'assets/images/icons/clock.gif';
+  static String kGiftIcon = 'assets/images/icons/gift.gif';
+  static String kIDIcon = 'assets/images/icons/id.gif';
 }
